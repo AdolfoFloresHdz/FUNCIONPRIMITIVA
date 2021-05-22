0e21cfab94e6d782bb1e905a7451f6aeefafab13
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-21 20:14:38 -0500

    Update funcionPrimitiva19_AdolfoFloresHernandez_3202.m

--- funcionPrimitiva19_AdolfoFloresHernandez_3202.m
+++ funcionPrimitiva19_AdolfoFloresHernandez_3202.m
@@ -1,14 +1,16 @@
 %Titulo : Funciones PRIMITIVAS
-% Descripción: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS Y SU GRAFICA
+% DescripciÃ³n: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS Y SU GRAFICA
 	% Autor:Adolfo Flores Hernandez ah7942469@gmail.com
 	% Fecha: 20/05/2021
 pkg load symbolic
 clear 
 clc 
 syms x;
-f = (x);
+f =x;
 F= inline(char(f));
-a=(-1);
-b=(4);
-F=int(f,a,b)
-plot (a:2:b,'r');
+liinf=1;
+lisu=4;
+y=int(f,x)
+disp ("Total es ");
+F=int(f,liinf,lisu)
+ezplot (y)
