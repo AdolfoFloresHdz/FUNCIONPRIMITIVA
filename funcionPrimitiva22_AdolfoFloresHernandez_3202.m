3b8cbc36caa55e5f5fe6af74f6594936859c4daa
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-21 20:16:56 -0500

    Update funcionPrimitiva22_AdolfoFloresHernandez_3202.m

--- funcionPrimitiva22_AdolfoFloresHernandez_3202.m
+++ funcionPrimitiva22_AdolfoFloresHernandez_3202.m
@@ -1,14 +1,16 @@
 %Titulo : Funciones PRIMITIVAS
-% Descripción: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS Y SU GRAFICA
+% DescripciÃ³n: Script PARA MOSTRAR EL RESULTADO funcion PRIMITIVAS Y SU GRAFICA
 	% Autor:Adolfo Flores Hernandez ah7942469@gmail.com
 	% Fecha: 20/05/2021
 pkg load symbolic
 clear 
 clc 
 syms x;
-f = (2*x^2)-(3*x);
+f =2*x^2-3*x;
 F= inline(char(f));
-a=(-3);
-b=(3);
-F=int(f,a,b)
-plot (a:1:b,'r');
+liinf=-3;
+lisu=3;
+y=int(f,x)
+disp ("RESULTADO: ");
+F=int(f,liinf,lisu)
+ezplot (y)
